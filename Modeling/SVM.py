c2309548fde8dfff4df748c707202f44d8263218
Abs0l0mb <57960571+Abs0l0mb@users.noreply.github.com>
2024-07-10 13:14:23 +0200

    graphs and improvement

--- Modeling/SVM.py
+++ Modeling/SVM.py
@@ -2,16 +2,19 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.metrics import classification_report, accuracy_score
-
-label_encoder_category = LabelEncoder()
-label_encoder_sentiment = LabelEncoder()
+from sklearn.metrics import precision_score, accuracy_score
+import numpy as np
 
 data = pd.read_csv('./AAPL_with_articles.csv')
+data['target'] = (data['close'].shift(1) > data['close']).astype(int)
 data = data.dropna()
+
+# Encode non numeric variables
+label_encoder_category = LabelEncoder()
+label_encoder_sentiment = LabelEncoder()
 data['dominant_category'] = label_encoder_category.fit_transform(data['dominant_category'].astype(str))
 data['dominant_sentiment'] = label_encoder_sentiment.fit_transform(data['dominant_sentiment'].astype(str))
-data['target'] = (data['close'].shift(1) > data['close']).astype(int)
+
 #data = data.drop(index=0).reset_index(drop=True)
 
 X = data.drop(columns=['datetime', 'target'])
@@ -20,15 +23,29 @@ y = data['target']
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+accuracies = []
+precisions = []
 
-svm_model = SVC(kernel='poly', verbose=True)
-svm_model.fit(X_train, y_train)
+kernel = 'sigmoid'
 
-y_pred = svm_model.predict(X_test)
+for _ in range(100):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=None)
 
-classification_report_output = classification_report(y_test, y_pred)
-accuracy = accuracy_score(y_test, y_pred)
+    svm_model = SVC(kernel=kernel, verbose=False)
+    svm_model.fit(X_train, y_train)
+    
+    y_pred = svm_model.predict(X_test)
+    
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    
+    accuracies.append(accuracy)
+    precisions.append(precision)
 
-print("Classification Report:\n", classification_report_output)
-print("Accuracy:", accuracy)
+average_accuracy = np.mean(accuracies)
+average_precision = np.mean(precisions)
+
+print("kernel : ", kernel)
+print("Av. Accuracy :", average_accuracy)
+print("Av. Precision :", average_precision)
+
--- Modeling/random_forest.py
+++ Modeling/random_forest.py
@@ -1,32 +1,45 @@
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import classification_report, accuracy_score
+from sklearn.metrics import classification_report, accuracy_score, precision_score
+import matplotlib.pyplot as plt
+
 
 label_encoder_category = LabelEncoder()
 label_encoder_sentiment = LabelEncoder()
 
 data = pd.read_csv('./AAPL_with_articles.csv')
+data['target'] = (data['close'].shift(1) > data['close']).astype(int)
 data = data.dropna()
+
 data['dominant_category'] = label_encoder_category.fit_transform(data['dominant_category'].astype(str))
 data['dominant_sentiment'] = label_encoder_sentiment.fit_transform(data['dominant_sentiment'].astype(str))
-data['target'] = (data['close'].shift(1) > data['close']).astype(int)
 #data = data.drop(index=0).reset_index(drop=True)
-
 
 X = data.drop(columns=['datetime', 'target'])
 y = data['target']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
-rf_model.fit(X_train, y_train)
+precision_scores = []
+accuracy_scores = []
+classification_reports = []
 
-y_pred = rf_model.predict(X_test)
+for i in range(0, 100):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-classification_report_output = classification_report(y_test, y_pred)
-accuracy = accuracy_score(y_test, y_pred)
+    rf_model = RandomForestClassifier(n_estimators=180, random_state=42)
+    rf_model.fit(X_train, y_train)
 
-print("Classification Report:\n", classification_report_output)
-print("Accuracy:", accuracy)
+    y_pred = rf_model.predict(X_test)
+
+    classification_reports.append(classification_report(y_test, y_pred, output_dict=True))
+    accuracy_scores.append(accuracy_score(y_test, y_pred))
+    precision_scores.append(precision_score(y_test, y_pred))
+
+total_precision = np.mean(precision_scores)
+total_accuracy = np.mean(accuracy_scores)
+
+print("Av. precision : ", total_precision)
+print("Av. accuracy : ", total_accuracy)
